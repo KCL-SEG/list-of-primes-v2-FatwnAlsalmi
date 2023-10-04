299f6d8d959f67c36f681bbd699553e52af427c2
FatwnAlsalmi <127515938+FatwnAlsalmi@users.noreply.github.com>
2023-10-04 15:32:08 +0100

    FinalPrimes.py

--- primes.py
+++ primes.py
@@ -1,6 +1,21 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
-
 def primes(number_of_primes):
     list = []
-    return list
+    if number_of_primes <= 0:
+        raise ValueError("Number is less than or equal zero!")
+    else:
+        number = 2
+
+        while len(list) < number_of_primes:
+            check = False
+            i = 2
+            while i < number:
+                if number % i == 0:
+                    check = True
+                    break
+                i += 1
+            if check == False:
+                list.append(number)
+            number +=1
+        return list
